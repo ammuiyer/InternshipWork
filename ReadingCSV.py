8ed4be8d709caf48198e5f1d3fdedea3f278df00
ammuiyer <ammu.iyer5@gmail.com>
2021-07-21 13:33:40 -0700

    readable df on total download and upload time per client

--- ReadingCSV.py
+++ ReadingCSV.py
@@ -7,10 +7,12 @@ df = pandas.read_csv('/Users/meenakshiiyer/Internship/SQL_report_4.csv')
 
 ip = df['clientIp']
 download = df['Download']
+upload = df['Upload']
+time = df['TimeUsage']
 row = len(ip)
 BigArray = []
 count = 0
-print('test')
+#print('test')
 #sorting into each client
 for col in range(row-1) :
 
@@ -21,16 +23,42 @@ for col in range(row-1) :
 for x in range(count+1) :
     newArray = []
     BigArray.append(newArray)   
-    print(BigArray)
+    #print(BigArray)
     
-count = 0   
+count = 0  
+ip_list = [] 
+
 for col in range(row-1) :
     BigArray[count].append(download[col])
-    print(BigArray[count])
+    #print(BigArray[count])
     if ip[col]!=ip[col+1]:
-       count = count+1
-   
-   
+        ip_list.append(ip[col])
+        count = count+1
+
+#print(ip_list)   
+#print(BigArray[0])  
+df_ip = pandas.DataFrame(columns=['TotalDownload', 'TotalUpload', 'TotalTime'], index=ip_list)
+#print(df_ip)
+total_download = 0
+total_upload = 0
+total_time = 0
+count = 0
+for col in range(row-1) :
+    total_download = total_download + download[col]
+    total_upload = total_upload + upload[col]
+    total_time = total_time + time[col]
+
+
+    if ip[col]!=ip[col+1] :
+        df_ip.iat[count,0] = total_download
+        df_ip.iat[count,1] = total_upload
+        df_ip.iat[count,2] = total_time
+        total_download = 0
+        total_upload = 0
+        total_time = 0
+        count = count+1
+print(df_ip)
+#print(df)
  
 
 
