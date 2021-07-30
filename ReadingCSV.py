5c026072d61dd749f2f96b1a3515398ef6eb65ea
ammuiyer <ammu.iyer5@gmail.com>
2021-07-30 11:06:57 -0700

    updated to megabytes

--- ReadingCSV.py
+++ ReadingCSV.py
@@ -93,26 +93,32 @@ for col in range(len(host)) :
 print(df_hosts)
 downld = df_ip['TotalDownload']
 upld = df_ip['TotalUpload']
+
+# convert to mega bytes
+temp = []
+temp2 = []
+for x in range(5):
+    temp.append((downld.iloc[x-1])/8000000)
+    temp2.append((upld.iloc[x-1])/8000000)
+
+downld = temp
+upld = temp2
 tim = df_ip['TotalTime']
 
-for x in range(5) : 
-    height = [downld[x-1], upld[x-1], tim[x-1]]
-    bars = ('Download', 'Upload', 'Time')
-    x_pos = numpy.arange(len(bars))
- 
+for x in range(1) : 
+    data = [downld, upld]
+    X  = numpy.arange(5)
+    fig = plt.figure()
+    plt.plot()
+    ax = fig.add_axes([0,0,1,1])
+    ax.bar(X + 0.00, data[0], color = 'b', width = 0.25)
+    ax.bar(X + 0.25, data[1], color = 'g', width = 0.25)
 
-    plt.bar(x_pos, height)
- 
-    
-    plt.title('IP: ' + ip_list[x-1])
-    plt.xlabel('Categories')
-    plt.ylabel('Values')
- 
-    
-    plt.xticks(x_pos, bars)
- 
-    
-    plt.show()
+    plt.xticks(X, ip_list)
+    plt.xlabel("Ips")
+    plt.ylabel("Bits")
+    plt.title("Each Ip Download and Upload")
+    plt.show() 
 downld = df_hosts['TotalDownload']
 upld = df_hosts['TotalUpload']
 tim = df_hosts['TotalTime']
@@ -135,3 +141,5 @@ for x in range(5) :
  
     
     plt.show()
+
+print(df)
